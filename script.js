30c145845c3fd21f1d60a32d0f7462568931c314
unknown <nazmiefg400@gmail.com>
2024-10-18 15:12:53 +0700

    UbahNoTelp

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.querySelector('.menu').addEventListener('click', function() {
 const whatsappButtons = document.querySelectorAll('.whatsappButton');
 whatsappButtons.forEach(button => {
   button.onclick = function() {
-    const phoneNumber = "6282328137106"; // Ganti dengan nomor telepon Anda
+    const phoneNumber = "6287790531215"; // Ganti dengan nomor telepon Anda
     const message = "Halo min, saya ingin bertanya tentang produk Anda."; // Ganti dengan pesan default Anda
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
